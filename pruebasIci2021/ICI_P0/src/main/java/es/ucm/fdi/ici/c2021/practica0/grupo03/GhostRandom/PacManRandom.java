2b1f63467420c780d3ae06c83c0da9e1f410b0a3
Posna <jomart17@ucm.es>
2020-10-06 16:35:30 +0200

    A ver si va

---
+++ pruebasIci2021/ICI_P0/src/main/java/es/ucm/fdi/ici/c2021/practica0/grupo03/GhostRandom/PacManRandom.java
@@ -0,0 +1,17 @@
+package es.ucm.fdi.ici.c2021.practica0.grupo03.GhostRandom;
+
+import java.util.Random;
+
+import pacman.controllers.PacmanController;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+
+public final class PacManRandom extends PacmanController {
+	private Random rnd = new Random();
+	private MOVE[] allMoves = MOVE.values();
+	
+	@Override
+	public MOVE getMove(Game game, long timeDue) {
+		return allMoves[rnd.nextInt(allMoves.length)];
+	}
+}
--- pruebasIci2021/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module pruebasIci2021 {
-}
