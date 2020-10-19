075d33ad22c3c99eb63b88af39e1c3c75d67910f
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2020-10-19 19:27:23 +0200

    Mi pana la señora pacman llega al segundo nivel

--- pruebasIci2021/ICI_P1/src/main/java/es/ucm/fdi/ici/c2021/practica1/grupo03/MsPacMan.java
+++ pruebasIci2021/ICI_P1/src/main/java/es/ucm/fdi/ici/c2021/practica1/grupo03/MsPacMan.java
@@ -10,13 +10,15 @@ import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 
 public final class MsPacMan extends PacmanController{
-	int limitGhost = 20;
+	int limitGhost = 15;
 	int limitPowerPill = 30;
 	double distance = Double.MAX_VALUE;
 	GHOST ghost = GHOST.BLINKY;
 	GHOST frontGhost = GHOST.BLINKY;
 	DM euristic = DM.EUCLID;
 	MOVE nextMove = MOVE.NEUTRAL; 
+	int ghostsIndex[] = new int[4];
+	
 	
 	//Este método indica si existe un fantasma que vaya de frente a pacman, teniendo en cuenta paredes
 	public boolean isGhostComingFromUp(Game game ,GHOST ghostType) {
@@ -62,71 +64,87 @@ public final class MsPacMan extends PacmanController{
 		/*
 		/*Busca el fantasma mas cercano*/
 		int i=0;
+		boolean ghostInFront=false;
+		distance = Double.MAX_VALUE;
+		frontGhost = null;
+		
 		for(GHOST ghostType : GHOST.values()) {
 			double d = game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghostType), euristic);
 			if(distance > d) {
 				distance = d;
 				ghost = ghostType;
 			}
-		}
-		//Mira si hay fantasmas que vengan de frente		
-		boolean ghostInFront=false;
-		for(GHOST ghostType: GHOST.values()) {
 			switch(game.getPacmanLastMoveMade()){
 			case UP:
 				if(game.getGhostLastMoveMade(ghostType)==MOVE.DOWN) {
 					if(isGhostComingFromUp(game, ghostType)) {
-					ghostInFront=true;
-					frontGhost = ghostType;
-					System.out.println("Fantasma Arriba");
+						ghostInFront=true;
+						frontGhost = ghostType;
+						System.out.println("Fantasma Arriba");
 					}
 				}
-					break;
+				break;
 			case DOWN:
 				if(game.getGhostLastMoveMade(ghostType)==MOVE.UP) {
 					if(isGhostComingFromDown(game,ghostType)) {
-					ghostInFront=true;
-					frontGhost = ghostType;
-					System.out.println("Fantasma abajo");
+						ghostInFront=true;
+						frontGhost = ghostType;
+						System.out.println("Fantasma abajo");
 					}
 				}
 				break;
 			case LEFT:
 				if(game.getGhostLastMoveMade(ghostType)==MOVE.RIGHT) {
 					if(isGhostComingFromLeft(game,ghostType)) {
-					ghostInFront=true;
-					frontGhost = ghostType;
-					System.out.println("Fantasma Izquierda");
+						ghostInFront=true;
+						frontGhost = ghostType;
+						System.out.println("Fantasma Izquierda");
 					}
 				}
 				break;
 			case RIGHT:
 				if(game.getGhostLastMoveMade(ghostType)==MOVE.LEFT) {
 					if(isGhostComingFromRight(game, ghostType)) {
-					ghostInFront=true;
-					frontGhost = ghostType;
-					System.out.println("Fantasma Derecha");
+						ghostInFront=true;
+						frontGhost = ghostType;
+						System.out.println("Fantasma Derecha");
 					}
 				}
 				break;
 			default:
-					break;
+				ghostInFront=false;
+				frontGhost = null;
+				break;
 			}
+			
+			ghostsIndex[i] = game.getGhostCurrentNodeIndex(ghostType);
+			i++;
 		}
-		if(ghostInFront && !game.isGhostEdible(ghost)) {
-			System.out.println("Cambiamos");
-			return game.getNextMoveAwayFromTarget(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(frontGhost), euristic);	
+		//Mira si hay fantasmas que vengan de frente		
+
+		
+		if(ghostInFront && !game.isGhostEdible(frontGhost)) {
+			if(game.isJunction(game.getPacmanCurrentNodeIndex())) {
+				System.out.println("Cambiamos");
+				nextMove = game.getPossibleMoves(game.getPacmanCurrentNodeIndex(), game.getPacmanLastMoveMade())[1];
+			} else {
+			System.out.println("No Cambiamos");
+
+			nextMove = game.getNextMoveAwayFromTarget(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(frontGhost), euristic);	
+			}
 		}
 
 		//Si hay un fantasma muy cerca
 		else if(!game.isGhostEdible(ghost) && distance <= limitGhost) {
-				return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghost), euristic);
+			System.out.println("Huimos mi gente");
+			nextMove = game.getNextMoveAwayFromTarget(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghost), euristic);
 			}
 			
 		
 		//Si es comestible
-		else if(game.isGhostEdible(ghost)) {
-			return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), 
+		else if(game.isGhostEdible(ghost) && distance <= limitGhost) {
+			System.out.println("Perseguimos");
+			nextMove = game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), 
 					game.getGhostCurrentNodeIndex(ghost), euristic);
 		}
 		
@@ -144,10 +162,11 @@ public final class MsPacMan extends PacmanController{
 				}
 			}
 			if(pillDist <= limitPowerPill) {
-				return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), 
+				System.out.println("MegaPAstis");
+				nextMove = game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), 
 						pillIndex, euristic);
 			}
-			
+			else {
 			pillDist = Double.MAX_VALUE;
 			for(int index : game.getActivePillsIndices()) {
 				double d = game.getDistance(game.getPacmanCurrentNodeIndex(), index, euristic);
@@ -156,10 +175,12 @@ public final class MsPacMan extends PacmanController{
 					pillIndex = index;
 				}
 			}
-			
-			return game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), 
+			System.out.println("PAstis");
+			nextMove = game.getNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), 
 					pillIndex, euristic);
+			}
 				
 		}
+		return nextMove;
 	}
 }
