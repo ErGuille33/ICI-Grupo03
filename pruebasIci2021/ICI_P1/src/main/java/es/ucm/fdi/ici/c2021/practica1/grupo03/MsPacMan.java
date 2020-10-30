9910173f470b9e96b360d2b6a19e25f21380fd6e
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2020-10-30 13:59:38 +0100

    Comentadillo

--- pruebasIci2021/ICI_P1/src/main/java/es/ucm/fdi/ici/c2021/practica1/grupo03/MsPacMan.java
+++ pruebasIci2021/ICI_P1/src/main/java/es/ucm/fdi/ici/c2021/practica1/grupo03/MsPacMan.java
@@ -25,13 +25,13 @@ public final class MsPacMan extends PacmanController{
 	int ghostsIndex[] = new int[4];
 	MOVE ghostsMoves[] = new MOVE[4];
 	
-	
+	//Busca la pill más cercana posible.
 	public int getClosestPossiblePill(Game game, ArrayList<MOVE> moves, int pacManNode) {
 		double pillDist=Double.MAX_VALUE;
 		int closestPill=-1;
 		for(int pill: game.getActivePillsIndices()) {
 			double dist=game.getDistance(pacManNode, pill, euristic);
-			
+			//SI el siguiente movimiento hacia la pill es posible, y la distancia es menor que la anterior.
 			if(moves.contains(game.getNextMoveTowardsTarget(pacManNode, pill, euristic)) && 
 					game.getDistance(pacManNode, pill, euristic)<pillDist) {
 				pillDist=dist;
@@ -52,27 +52,29 @@ public final class MsPacMan extends PacmanController{
 			System.out.println("");
 		
 		ArrayList<MOVE>possibleMoves=new ArrayList<MOVE>();
-		
+		//Guarda los movimientos posibles
 		for(MOVE m: moves) {
 			possibleMoves.add(m);
 		}
 		
 		ArrayList<GHOST> inRangeToEat= new ArrayList<GHOST>();
-		
+		//Recorre los fantasmas 
 		for(GHOST ghostType : GHOST.values()) {
 				
 			int ghostNode = game.getGhostCurrentNodeIndex(ghostType);
 				
 			double d = game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghostType), euristic);
-				
+			//Si estan dentro del radio, no son comestibles.
 			if(!game.isGhostEdible(ghostType) && d < focusRadius) {
+				//Si el estan dentro del radio maximo siguiendo el camino
 				if(game.getShortestPathDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(ghostType))
 						<= maxPathDistance){
+					//Borran los posibles movimientos que le dirijan hacia un fantasma demasiado cercano que vaya hacia él
 					if(game.getGhostLastMoveMade(ghostType) == game.getNextMoveTowardsTarget(ghostNode, pacManNode, euristic)) {
 						possibleMoves.remove(game.getNextMoveTowardsTarget(pacManNode, ghostNode, euristic));
 					}
 				}
-			}
+			}//Si es comestible y está cerca.
 			else if(game.isGhostEdible(ghostType) && d <= eatDistance) {
 				inRangeToEat.add(ghostType);
 			}
@@ -80,13 +82,13 @@ public final class MsPacMan extends PacmanController{
 		
 		if(game.isJunction(pacManNode))
 			System.out.println(possibleMoves);
-		
+		//Pildora mas cercana
 		int pill = getClosestPossiblePill(game, possibleMoves, pacManNode);
 		
 		boolean eatGhost=false;
 		
 		MOVE moveToGhost=MOVE.NEUTRAL;
-		
+		//Mira si los comestibles estan en un rango de movimientos posible
 		for(GHOST g: inRangeToEat) {
 			moveToGhost=game.getNextMoveTowardsTarget(pacManNode, game.getGhostCurrentNodeIndex(g), euristic);
 			if(possibleMoves.contains(moveToGhost)) {
@@ -97,13 +99,13 @@ public final class MsPacMan extends PacmanController{
 		
 		if(possibleMoves.isEmpty())
 			nextMove=MOVE.NEUTRAL;
-		
+		//Va hacia el fantasma
 		else if(eatGhost) 
 			nextMove=moveToGhost;
-		
+		//Va hacia pildora
 		else if(pill !=-1)
 			nextMove=game.getNextMoveTowardsTarget(pacManNode, pill, euristic);
-		
+		//Movimiento random
 		else {
 			int siz = possibleMoves.size();
 				
